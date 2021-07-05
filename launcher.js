304581ee6ac5f9b30be5b4910141cfbc8762d6ed
Taarana <81751883+Taarana@users.noreply.github.com>
2021-07-05 12:21:57 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,33 @@
+class launcher
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	fly()
+	{
+     this.launcher.bodyA=null;
+	}
+	display()
+	{
+			var launcherPos=this.body.position;		
+			push()
+			translate(launcherPos.x, launcherPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
